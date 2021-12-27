655d0543cbfdf9df8241d3e78fc71bebc46c8312
venzel <eneas.eng@yahoo.com>
2021-12-27 16:59:24 -0300

    feat: cria interceptadores de logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './configs/winston.config';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 import { typeOrmConfig } from './configs/typeorm.config';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { mailerConfig } from './configs/mailer.config';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, AuthModule],
+    imports: [
+        TypeOrmModule.forRoot(typeOrmConfig),
+        WinstonModule.forRoot(winstonConfig),
+        MailerModule.forRoot(mailerConfig),
+        UserModule,
+        AuthModule,
+    ],
     controllers: [],
-    providers: [],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: LoggerInterceptor,
+        },
+    ],
 })
 export class AppModule {}
